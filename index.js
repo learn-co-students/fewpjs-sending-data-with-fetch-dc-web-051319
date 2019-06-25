569fd06d92d9b1a7376dd0dbb530a709599f34f7
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-06-25 14:24:21 -0400

    Used a fetch request to post data to the server and show it on the DOM

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const body = document.querySelector('body')
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => document.body.innerHTML= data["id"])
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
+
+submitData("Yeti", "abominablesnowman@gmail.com")
