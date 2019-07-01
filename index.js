34d59695dd8357433e0feb68ae0daa00bb66f126
newtypeV2 <gianlibunao@gmail.com>
2019-07-01 08:48:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Add your code here
+
+// function submitData(uName, eAddress){
+//     // let formData = {
+//     //     userName: uName,
+//     //     emailAddress: eAddress
+//     //   };
+
+//       let configObj = {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "Accept": "application/json"
+//         },
+//         body: JSON.stringify({uName,eAddress})
+//       };
+
+//     fetch("http://localhost:3000/users", configObj)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(object) {
+//     document.body.innerHTML = object[ "id" ];
+//   })
+//   .catch(function(error) {
+//     document.body.innerHTML = error.message
+//   });
+
+// }
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
+  
+  
+
+  
